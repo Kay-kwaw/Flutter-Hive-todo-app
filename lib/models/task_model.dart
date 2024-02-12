044888c739a8dbb0ea47a1b2e36afce368312edc
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-02-12 11:40:32 +0000

    models

---
+++ lib/models/task_model.dart
@@ -0,0 +1,9 @@
+class Todo {
+  final String title;
+  final String content;
+  final bool isCompleted;
+
+  const Todo({
+    required this.title, required this.content, required this.isCompleted
+  });
+}
