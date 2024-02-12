9e3bb1eefadf098adc25edc73ad76ff2a075f641
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-02-12 12:08:25 +0000

    hive_dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netwoking/contants.dart';
 import 'package:flutter_netwoking/homepage.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
